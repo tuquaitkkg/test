0c20886501b59ad3d275e7e7b50c14311c03927e
Le Trung Duc <duc.lt@Duys-Mac-mini.local>
2016-10-19 17:10:02 +0700

    Add new

---
+++ eCompleteContact/eCompleteContact/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  eCompleteContact
+//
+//  Created by Le Trung Duc on 10/18/16.
+//  Copyright © 2016 3Si. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ eCompleteContact/eCompleteContact/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  eCompleteContact
+//
+//  Created by Le Trung Duc on 10/18/16.
+//  Copyright © 2016 3Si. All rights reserved.
+//
+
+import Cocoa
+import RxSwift
+import RxCocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
