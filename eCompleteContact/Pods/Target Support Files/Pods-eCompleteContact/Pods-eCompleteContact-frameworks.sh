f747b99b16a59c9e1516a613e22a32643ffb9462
Le Trung Duc <duc.lt@Duys-Mac-mini.local>
2016-10-21 10:29:24 +0700

    Add new file

--- eCompleteContact/Pods/Target Support Files/Pods-eCompleteContact/Pods-eCompleteContact-frameworks.sh
+++ eCompleteContact/Pods/Target Support Files/Pods-eCompleteContact/Pods-eCompleteContact-frameworks.sh
@@ -85,6 +85,8 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxAlamofire/RxAlamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
@@ -92,6 +94,8 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxAlamofire/RxAlamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
---
+++ eCompleteContact/eCompleteContact/AddContactViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddContactViewController.swift
+//  eCompleteContact
+//
+//  Created by Le Trung Duc on 10/21/16.
+//  Copyright © 2016 3Si. All rights reserved.
+//
+
+import Foundation
