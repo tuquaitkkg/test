1516887d42f1975f78e886f5aee6330c6b098782
DUCLT <tuquaitkkg01@gmail.com>
2016-11-05 10:49:49 +0700

    Add Base View

---
+++ eCompleteContact/eCompleteContact/ViewController/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseViewController.swift
+//  eCompleteContact
+//
+//  Created by DUCLT on 11/5/16.
+//  Copyright © 2016 3Si. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class BaseViewController: NSSplitViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+    }
+}
